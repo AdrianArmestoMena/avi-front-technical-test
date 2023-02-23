0ee48fd695dc360a8bf321cc578eb6b1e18685be
Adrian Armesto <armestomena@gmail.com>
2023-02-23 17:15:12 +0100

    Create condition form component

---
+++ src/utils/conditions.ts
@@ -0,0 +1,21 @@
+export const mechanicConditions = [
+  "En espera",
+  "Diagnosis",
+  "Espera recambio",
+  "Inicio Reparacion",
+  "Pruebas",
+  "Limpieza",
+  "Acabado",
+];
+
+export const bodyWorkConditions = [
+  "En espera",
+  "Peritado",
+  "Recambio Solicitado",
+  "Desmontaje",
+  "Reparacion Carroceria",
+  "Reparacion Pintura",
+  "Montaje",
+  "Limpieza",
+  "Acabado",
+];
