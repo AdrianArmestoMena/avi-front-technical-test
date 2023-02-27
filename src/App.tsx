b86af9ecf262955139a9c1ba5ad574391e35ed06
Adrian Armesto <armestomena@gmail.com>
2023-02-27 10:34:11 +0100

    Add header and outlet on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./components/Header/Header";
 
 const App = () => {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Header />
+      <Outlet />
+    </div>
+  );
 };
 
 export default App;
