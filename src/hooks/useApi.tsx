4831d02d03e0c0272fd83c09667e554a2e5e2375
Adrian Armesto <armestomena@gmail.com>
2023-03-01 12:25:20 +0100

    Use ui action on use api

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -1,17 +1,27 @@
-import axios, { AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosResponse } from "axios";
 import { useCallback, useContext } from "react";
 import { loadServiceActionCreator } from "../store/actions/serviceActions/serviceActionCreators";
+import {
+  closeModalActionCreator,
+  openConditionActionCreator,
+  openDateActionCreator,
+  openErrorActionCreator,
+  openLoadingActionCreator,
+} from "../store/actions/uiActions/uiActionCreators";
 import ServiceContext from "../store/contexts/ServiceContext/ServiceContext";
-import Service, { Response } from "../types/Service";
+import UiContext from "../store/contexts/uiContext/UiContext";
+import Service, { Condition, Response } from "../types/Service";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 const useApi = () => {
-  const { dispatch } = useContext(ServiceContext);
+  const { dispatch, service } = useContext(ServiceContext);
+  const { dispatch: uiDispatch } = useContext(UiContext);
 
   const loadService = useCallback(
     async (id: string) => {
       try {
+        uiDispatch(openLoadingActionCreator());
         const {
           data: { serviceData },
         }: AxiosResponse<Response> = await axios.get(`${apiUrl}service/${id}`);
@@ -39,20 +49,41 @@ const useApi = () => {
           phone: contact_phone,
           type: service_type,
           brand,
-          dateFormat: Date.now(),
         };
-        console.log(await serviceData);
+
         dispatch(loadServiceActionCreator(await newService));
-      } catch {
-        console.log(apiUrl);
+        uiDispatch(closeModalActionCreator());
+      } catch (error) {
+        if ((error as AxiosError).message === "Network Error") {
+          uiDispatch(
+            openErrorActionCreator(
+              "Estamos teniendo algunos problemas, intentalo más tarde."
+            )
+          );
+        }
+        if (error instanceof AxiosError) {
+          const errorObject = JSON.parse(
+            (error as AxiosError).request.response
+          );
+
+          uiDispatch(
+            openErrorActionCreator(
+              errorObject.error
+                ? errorObject.error
+                : "Estamos teniendo algunos problemas, intentalo más tarde."
+            )
+          );
+          return;
+        }
       }
     },
-    [dispatch]
+    [dispatch, uiDispatch]
   );
 
   const updateConditionService = useCallback(
-    async (id: string, newConditions: string) => {
+    async (id: string, newConditions: Condition) => {
       try {
+        uiDispatch(openLoadingActionCreator());
         const response: AxiosResponse<Response> = await axios.put(
           `${apiUrl}service/updateCondition/${id}`,
           { condition: newConditions },
@@ -63,19 +94,23 @@ const useApi = () => {
           }
         );
         if (response.status === 404) {
-          throw new Error();
+          throw new Error(response.data.error);
         }
-        loadService(id);
-      } catch {
-        console.log(apiUrl);
+        dispatch(
+          loadServiceActionCreator({ ...service, condition: newConditions })
+        );
+        uiDispatch(openConditionActionCreator(newConditions));
+      } catch (error: unknown) {
+        uiDispatch(openErrorActionCreator((error as Error).message));
       }
     },
-    [loadService]
+    [dispatch, service, uiDispatch]
   );
 
   const updateDateService = useCallback(
     async (id: string, date: string) => {
       try {
+        uiDispatch(openLoadingActionCreator());
         const response: AxiosResponse<Response> = await axios.put(
           `${apiUrl}service/updateDate/${id}`,
           { date },
@@ -88,12 +123,34 @@ const useApi = () => {
         if (response.status === 404) {
           throw new Error();
         }
-        loadService(id);
-      } catch {
-        console.log(apiUrl);
+        await loadService(id);
+        uiDispatch(openDateActionCreator(date));
+      } catch (error) {
+        if ((error as AxiosError).message === "Network Error") {
+          uiDispatch(
+            openErrorActionCreator(
+              "Estamos teniendo algunos problemas, intentalo más tarde."
+            )
+          );
+        }
+
+        if (error instanceof AxiosError) {
+          const errorObject = JSON.parse(
+            (error as AxiosError).request.response
+          );
+
+          uiDispatch(
+            openErrorActionCreator(
+              errorObject.error
+                ? errorObject.error
+                : "Estamos teniendo algunos problemas, intentalo más tarde."
+            )
+          );
+          return;
+        }
       }
     },
-    [loadService]
+    [loadService, uiDispatch]
   );
   return { loadService, updateConditionService, updateDateService };
 };
