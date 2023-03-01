63d7be95592d8b491ca61e2fa52aadd80eba1220
Adrian Armesto <armestomena@gmail.com>
2023-03-01 12:38:40 +0100

    Solve margins on calendar modal

--- src/components/calendarModal/calendarModal.Style.tsx
+++ src/components/calendarModal/calendarModal.Style.tsx
@@ -1,21 +1,30 @@
 import styled from "styled-components";
 
 const CalendarFormStyled = styled.section`
+  display: flex;
+  flex-direction: column;
   width: fit-content;
-  margin: 20px;
   align-items: center;
   gap: 0.5rem;
-
+  margin: 0;
+  padding: 1rem;
   .calendar-form {
     &__label {
+      gap: 0.5rem;
+      padding: 01rem;
+      margin: 0;
       white-space: nowrap;
       text-align: center;
       background-color: aliceblue;
       border: 2px solid black;
       background-color: #00bfb2;
-      padding: 0.5rem;
-      font-size: 1.5rem;
+      font-size: 1rem;
       border: none;
+      display: flex;
+      flex-direction: column;
+      @media (min-width: 700px) {
+        font-size: 1.5rem;
+      }
       &:hover {
         background-color: #53d3cb;
       }
